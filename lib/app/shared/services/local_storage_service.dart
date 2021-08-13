8a7354313fbe33ed32e29eb83f16775ad08d0be7
Eduardo Sant'Ana <edusantsales@gmail.com>
2021-08-12 22:16:19 -0300

    Adicionado a classe de serviço LocalStorageService para armazenar informações locais no app atráves do package SharedPreferences.

---
+++ lib/app/shared/services/local_storage_service.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:pokedex/app/shared/interfaces/interfaces.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LocalStorageService implements ILocalStorage {
+  @override
+  Future delete(String key) async {
+    var shared = await SharedPreferences.getInstance();
+    return shared.remove(key);
+  }
+
+  @override
+  Future get(String key) async {
+    var shared = await SharedPreferences.getInstance();
+    return shared.get(key);
+  }
+
+  @override
+  Future put(String key, value) async {
+    var shared = await SharedPreferences.getInstance();
+    if (value is bool) {
+      shared.setBool(key, value);
+    } else if (value is int) {
+      shared.setInt(key, value);
+    } else if (value is double) {
+      shared.setDouble(key, value);
+    } else if (value is String) {
+      shared.setString(key, value);
+    } else {
+      shared.setString(key, json.encode(value));
+    }
+  }
+}
