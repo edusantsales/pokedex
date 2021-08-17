6faf1c35dfdb8bdc42f27ceadbe374d84659e713
Eduardo Sant'Ana <edusantsales@gmail.com>
2021-08-17 18:07:45 -0300

    Renomeado o atributo index para pokemonIndex.
    Adicionado os métodos loadHomePage e interval.

--- lib/app/modules/detail/detail_view_model.dart
+++ lib/app/modules/detail/detail_view_model.dart
@@ -1,11 +1,28 @@
-import 'package:flutter/material.dart';
+import 'dart:ui';
+
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:pokedex/app/shared/entities/entities.dart';
 import 'package:rx_notifier/rx_notifier.dart';
 
 class DetailViewModel {
-  var index = 0;
+  var pokemonIndex = 0;
   var pokemon = RxNotifier(
-    Pokemon(id: 0, image: Container(), name: "", types: []),
+    Pokemon(id: 0, imageUrl: "", name: "", types: []),
   );
   var pokemons = RxList(<Pokemon>[]);
+
+  VoidCallback loadHomePage() {
+    return () {
+      Modular.to.pop();
+    };
+  }
+
+  double interval(double lower, double upper, double progress) {
+    assert(lower < upper);
+
+    if (progress > upper) return 1.0;
+    if (progress < lower) return 0.0;
+
+    return ((progress - lower) / (upper - lower)).clamp(0.0, 1.0);
+  }
 }
